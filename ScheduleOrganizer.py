7350429fea157a4adf838579d12d811cb228db82
ImmortalCeltic <59520287+ImmortalCeltic@users.noreply.github.com>
2022-11-17 16:24:16 -0800

    Re-did GUI Functionality as standalone application, some excel interfacing tested

--- ScheduleOrganizer.py
+++ ScheduleOrganizer.py
@@ -1,6 +1,8 @@
 from gooey import Gooey, GooeyParser
 import os
+import mkl
 import json
+import openpyxl as pyxl
 
 @Gooey(program_name="Venue Grid Generator")
 def parse_args():
@@ -20,43 +22,76 @@ def parse_args():
     parser = GooeyParser(description='Produce a Venue Schedule')
     #Creating Dimensions for schedule grid
     #Number of volunteers at venue
-    parser.add_argument('Number of Volunteers',
+    #Include functionality of selecting number of volunteer slots for each position
+    #Include functionality to pick a selection of dates
+    #Include functionality of specifying a shift time, for volunteers and supervisors
+    parser.add_argument('--Number_of_Volunteers',
                         action='store',
                         widget='IntegerField',
-                        default=stored_args.get('Number_of_Volunteers'),
+                        default=1,
                         help="Specify the number of volunteers that will be present")
     #Venue Name
-    parser.add_argument('Venue Name',
+    parser.add_argument('Venue_Name',
                         action='store',
                         default=stored_args.get('Venue_Name'),
                         help='Specify the name of the venue')
     #List of venue positions
-    parser.add_argument('Volunteer/Supervisor Positions',
+    parser.add_argument('Volunteer_Supervisor_Positions',
                         choices=['Beer Garden','Front of House','Green Team','Hospitality','Merchandise','Staging','Security'],
                         action='store',
                         default=stored_args.get('Volunteer/Supervisor Positons'),
                         widget='Listbox',
-                        nargs="*",
+                        nargs="+",
                         metavar="Volunteer/Supervisor Positons",
                         help='Specify the work positons for this venue')
     #Type of supervisors at venue
-    parser.add_argument('Volunteer/Supervisor Positions',
+    parser.add_argument('Supervisor_Positions',
                         choices=['Beer Garden','Front of House','Green Team','Hospitality','Merchandise','Staging','Security'],
                         action='store',
-                        default=stored_args.get('Volunteer/Supervisor Positons'),
+                        default=stored_args.get('Supervisor_Positons'),
                         widget='Listbox',
-                        nargs="*",
-                        metavar="Volunteer/Supervisor Positons",
+                        nargs="+",
+                        metavar="Supervisor Positons",
                         help='Specify the work positons for this venue')
-    parser.add_argument('Number of Shows',
+    parser.add_argument('--Number_of_Shows',
                         action='store',
-                        default=stored_args.get('Number_of_Shows'),
+                        default=1,
                         widget='IntegerField',
                         help="Specify the number of shows happening at this venue")
-    args = parser.parse_args()
+    #Return parser function to main                    
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    conf = parse_args()
     print("Creating Venue Schedule Grid")
+    wb = pyxl.load_workbook("C:\\Users\\12508\\Documents\\ProgrammingStuff\\VolunteerOrganizer\\Test.xlsm", keep_vba=True)
+    sheet = wb.active
+    sheet.title = 'Hello World'
+    wb.save("Test.xlsm")
+    args = parse_args()
+    print(args.Number_of_Volunteers)
+    print(args.Venue_Name)
+    print(args.Volunteer_Supervisor_Positions)
+    print(args.Supervisor_Positions)
+    print(args.Number_of_Shows)
     #Pass all variables into excel spreadsheet generator program
+
+    #Make Venue Name Header
+
+    #Generate columns based on number of performance days, length based on # volunteer positions and supervisor positions;
+    #cross-examine Supervisor_Positions in cell exclusion case
+
+    #Create cell formatting (size, colour, etc)
+
+    #Insert volunteer positions into designated cell
+
+    #Insert Dates into each cell
+
+
+    sheet['A1'] = args.Number_of_Volunteers
+    sheet['A2'] = args.Venue_Name
+    sheet['A3'] = str(args.Volunteer_Supervisor_Positions)
+    sheet['A4'] = str(args.Supervisor_Positions)
+    sheet['A5'] = args.Number_of_Shows
+    wb.save("Test.xlsm")
+        
     print("Done")
